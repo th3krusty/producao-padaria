29eb7d3bf43cd128cfefde7f5a56fe32f31371a2
th3krusty <th3krusty@gmail.com>
2026-08-16 19:58:42 -0300

    Change base path in Vite config to root

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // quando o app é acessado em tkone.com.br/producao via rewrite do Vercel.
 export default defineConfig({
   plugins: [react()],
-  base: "/producao-app/",
+  base: "/",
 });
